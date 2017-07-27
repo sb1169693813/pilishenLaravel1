19413067f7490a876c1c65f5cfe0f5f6c87228f1
vagrant <446352377@qq.com>
2017-07-27 15:30:29 +0000

    vuejs step2

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,8 +2,13 @@
 
 @section('content')
   <div id="app" class="container">
-    <h1>@{{ message }}</h1>
-    <input type="text" name="" value="" v-model="message">
+    <!-- <h1>@{{ message }}</h1> -->
+    <ul class="list-group">
+      <li class="list-group-item" v-for="step in steps">@{{ step.name }}</li>
+      <input type="text" name="" value="" v-model="newStep" @keyup.enter="addStep" class="form-control">
+    </ul>
+
+    @{{ $data}}
   </div>
 @endsection
 
@@ -13,7 +18,19 @@
       var app = new Vue({
         el: "#app",
         data: {
-          message:"Hello sun"
+          // message:"Hello sun"
+          steps:[
+            {name:"fix bug",completed:false},
+            {name:"meeting",completed:false},
+          ],
+          newStep:''
+        },
+        methods:{
+          addStep:function(){
+            //alert('输入');
+            this.steps.push({name:this.newStep,completed:false});
+            this.newStep = '';
+          }
         }
       });
   </script>
